27f0ccd8b0861ad59df71f057dad87ed42007c1c
unknown <naufalbakri122@gmail.com>
2022-06-14 01:00:38 +0800

    Move some events to everyone

--- cogs/everyone.py
+++ cogs/everyone.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import time
 from utils import *
 
+DEVELOPER_ID = 319485284465115147
+
 
 class everyone(commands.Cog):
     def __init__(self, client: commands.Bot):
@@ -27,6 +29,50 @@ class everyone(commands.Cog):
             color=SILVER
         )
         await message.edit(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.content == self.client.user.mention:
+            prefix = get_prefix(self.client, message)
+            developer = await self.client.fetch_user(DEVELOPER_ID)
+
+            title = 'Server settings information'
+            text = f'''Server prefix: `{prefix}`
+            Server ID: `{message.guild.id}`
+
+            Join a voice channel and `{prefix}play` a song.
+            Type `{prefix}help` for the list of commands.'''
+            # [Invite]({INVITE_LINK})
+
+            embed = discord.Embed(title=title, description=text, color=SILVER)
+            embed.set_footer(text=f'Developed by {developer}')
+            await message.channel.send(embed=embed)
+        await self.client.process_commands(message)
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
+        ignored = (commands.NoPrivateMessage, commands.DisabledCommand, commands.CheckFailure,
+                   commands.CommandNotFound, discord.HTTPException, commands.NotOwner)
+        error = getattr(error, 'original', error)
+        if isinstance(error, ignored):
+            pass
+        elif isinstance(error, commands.MissingPermissions):
+            text = str(error)
+            # You are missing Manage Messages and Manage Nicknames permission(s) to run this command.
+            text = text.replace('(s)', '')
+            text = text[:-32] + '`' + text[-32:]
+            text = text[:16] + '`' + text[16:]
+            text = text.replace(' and ', '` and `')
+            # You are missing `Manage Messages` and `Manage Nicknames` permission to run this command.
+            await send_notice(ctx, text, notice_type=WARNING)
+        elif isinstance(error, commands.MissingRequiredArgument):
+            text = f'Some argument(s) are not provided.\nHave a look at `{get_prefix(self.client, ctx.message)}help {ctx.command.name}`'
+            await send_notice(ctx, text)
+        elif isinstance(error, commands.UserInputError):
+            text = f'Invalid argument provided.\nHave a look at `{get_prefix(self.client, ctx.message)}help {ctx.command.name}`'
+            await send_notice(ctx, text)
+        else:
+            print(error.__class__.__name__, ' > ', error)
 
 
 def setup(client: commands.Bot):
--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-DEVELOPER_ID = 319485284465115147
 
 client = commands.Bot(
     command_prefix=get_prefix,
@@ -38,52 +37,6 @@ async def on_guild_join(guild: discord.Guild):
 async def on_guild_remove(guild: discord.Guild):
     remove_info(client, guild)
     save_info(client)
-
-
-@client.event
-async def on_message(message: discord.Message):
-    if message.content == client.user.mention:
-        prefix = get_prefix(client, message)
-        developer = await client.fetch_user(DEVELOPER_ID)
-
-        title = 'Server settings information'
-        text = f'''Server prefix: `{prefix}`
-        Server ID: `{message.guild.id}`
-
-        Join a voice channel and `{prefix}play` a song.
-        Type `{prefix}help` for the list of commands.'''
-        # [Invite]({INVITE_LINK})
-
-        embed = discord.Embed(title=title, description=text, color=SILVER)
-        embed.set_footer(text=f'Developed by {developer}')
-        await message.channel.send(embed=embed)
-    await client.process_commands(message)
-
-
-@client.event
-async def on_command_error(ctx: commands.Context, error: commands.CommandError):
-    ignored = (commands.NoPrivateMessage, commands.DisabledCommand, commands.CheckFailure,
-               commands.CommandNotFound, discord.HTTPException, commands.NotOwner)
-    error = getattr(error, 'original', error)
-    if isinstance(error, ignored):
-        pass
-    elif isinstance(error, commands.MissingPermissions):
-        text = str(error)
-        # You are missing Manage Messages and Manage Nicknames permission(s) to run this command.
-        text = text.replace('(s)', '')
-        text = text[:-32] + '`' + text[-32:]
-        text = text[:16] + '`' + text[16:]
-        text = text.replace(' and ', '` and `')
-        # You are missing `Manage Messages` and `Manage Nicknames` permission to run this command.
-        await send_notice(ctx, text, notice_type=WARNING)
-    elif isinstance(error, commands.MissingRequiredArgument):
-        text = f'Some argument(s) are not provided.\nHave a look at `{get_prefix(client, ctx.message)}help {ctx.command.name}`'
-        await send_notice(ctx, text)
-    elif isinstance(error, commands.UserInputError):
-        text = f'Invalid argument provided.\nHave a look at `{get_prefix(client, ctx.message)}help {ctx.command.name}`'
-        await send_notice(ctx, text)
-    else:
-        print(error.__class__.__name__, ' > ', error)
 
 
 @client.command()
