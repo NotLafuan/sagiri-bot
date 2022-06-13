05dd187d6d3f4e53cfeef0fca0d7a95b2f7a6814
unknown <naufalbakri122@gmail.com>
2022-06-14 01:02:52 +0800

    Update everyone.py

--- cogs/everyone.py
+++ cogs/everyone.py
@@ -10,26 +10,6 @@ class everyone(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
 
-    @commands.command(help='<text to repeat>', description='Make Sagiri say something.\n`[Everyone]`')
-    async def say(self, ctx: commands.Context, *, query: str):
-        await ctx.message.delete()
-        await ctx.send(query)
-
-    @commands.group(name='ping', aliases=['latency', 'lat'], invoke_without_command=True, help='', description='Shows the latency of the bot.\n`[Everyone]`')
-    async def ping(self, ctx: commands.Context):
-        await send_notice(ctx, f'Pong! `{round(self.client.latency * 1000)}ms`', notice_type=MESSAGE)
-
-    @ping.command(name='ws', aliases=['websocket'], help='', description='Shows the websocket latency of the bot.\n`[Everyone]`')
-    async def ping_ws(self, ctx: commands.Context):
-        start_time = time.perf_counter()
-        message = await send_notice(ctx, f'Pong!', notice_type=MESSAGE)
-        ping = (time.perf_counter() - start_time) * 1000
-        embed = discord.Embed(
-            description=f'Pong! `{int(ping)}ms`',
-            color=SILVER
-        )
-        await message.edit(embed=embed)
-
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if message.content == self.client.user.mention:
@@ -73,6 +53,26 @@ class everyone(commands.Cog):
             await send_notice(ctx, text)
         else:
             print(error.__class__.__name__, ' > ', error)
+
+    @commands.command(help='<text to repeat>', description='Make Sagiri say something.\n`[Everyone]`')
+    async def say(self, ctx: commands.Context, *, query: str):
+        await ctx.message.delete()
+        await ctx.send(query)
+
+    @commands.group(name='ping', aliases=['latency', 'lat'], invoke_without_command=True, help='', description='Shows the latency of the bot.\n`[Everyone]`')
+    async def ping(self, ctx: commands.Context):
+        await send_notice(ctx, f'Pong! `{round(self.client.latency * 1000)}ms`', notice_type=MESSAGE)
+
+    @ping.command(name='ws', aliases=['websocket'], help='', description='Shows the websocket latency of the bot.\n`[Everyone]`')
+    async def ping_ws(self, ctx: commands.Context):
+        start_time = time.perf_counter()
+        message = await send_notice(ctx, f'Pong!', notice_type=MESSAGE)
+        ping = (time.perf_counter() - start_time) * 1000
+        embed = discord.Embed(
+            description=f'Pong! `{int(ping)}ms`',
+            color=SILVER
+        )
+        await message.edit(embed=embed)
 
 
 def setup(client: commands.Bot):
